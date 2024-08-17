58a65cbd425aae54d1abe9b16f883cf767fe8d0c
Harshal Yallewar <harshal.yallewar@globallogic.com>
2024-08-17 23:12:42 +0530

    change to js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
-
 dotenv.config();
 
 const app = express();
@@ -11,15 +10,12 @@ app.use(
   cors({
     origin: "*", 
   })
-  );
-
-  app.use(express.json());
-  
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/webhook', (req: Request, res: Response) => {
+app.post('/webhook', (req, res) => {
   console.log(JSON.stringify(req.body, null, 2));
 
   const fulfillmentResponse = {
@@ -46,8 +42,8 @@ app.use((req, res, next) => {
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    console.log(req.body);
-  res.send("Harshal").sendStatus(200);
+  console.log(req.body);
+  res.send("Harshal").status(200);
 });
 
 app.listen(PORT, () => {
